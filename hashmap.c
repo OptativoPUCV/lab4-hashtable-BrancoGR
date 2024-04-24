0a815f00e01b5338b8dd173fbd6d81f60c8b4ff7
BrancoGR <163342223+BrancoGR@users.noreply.github.com>
2024-04-24 22:01:06 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -127,7 +127,7 @@ Pair * nextMap(HashMap * map)
       if (map->buckets[i] != NULL)
       {
         map->current = i;
-        return map->buckets[i];
+        return map->buckets[i++];
       }
     }
 
