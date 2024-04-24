3ab34dbbf36b6b2e1570c7cad4eab82604307c29
BrancoGR <163342223+BrancoGR@users.noreply.github.com>
2024-04-24 21:56:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -109,7 +109,7 @@ Pair * searchMap(HashMap * map,  char * key)
 
 Pair * firstMap(HashMap * map)
 {
-  for (long i = 0; i < map->capacity; i++)
+  for (int i = 0; i < map->capacity; i++)
     {
       if (map->buckets[i] != NULL)
       {
@@ -117,7 +117,6 @@ Pair * firstMap(HashMap * map)
         return map->buckets[i];
       }
     }
-
     return NULL;
 }
 
