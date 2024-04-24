be497da45e73e802ea122da013d0c76b686b2ab3
BrancoGR <163342223+BrancoGR@users.noreply.github.com>
2024-04-24 21:52:20 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -75,7 +75,11 @@ HashMap * createMap(long capacity)
   return map;  
 }
 
-void eraseMap(HashMap * map,  char * key) {    
+void eraseMap(HashMap * map,  char * key)
+{
+  int posicion = hash(key,map->capacity);
+
+  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL)
 
 
 }
@@ -84,7 +88,7 @@ Pair * searchMap(HashMap * map,  char * key)
 {   
   int posicion = hash(key,map->capacity);
   
-  while(map->buckets[posicion] != NULL)
+  while(map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL)
     {
       if(is_equal(map->buckets[posicion]->key,key))
       {
