74d4c07c497a0e666edfcd1a2837b8ff1046fe7c
BrancoGR <163342223+BrancoGR@users.noreply.github.com>
2024-04-24 22:11:14 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -63,7 +63,7 @@ void enlarge(HashMap * map)
   enlarge_called = 1; //no borrar (testing purposes)
   Pair** aux = map->buckets;
   map->buckets = (Pair**)malloc(sizeof(Pair*)*map->capacity*2);
-  for(int i = 0; i < map->capacity*2; i++)
+  for(int i = 0; i < map->capacity; i++)
     {
       if(aux[i] != NULL)
       {
